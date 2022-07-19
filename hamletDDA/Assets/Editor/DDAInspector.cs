5b2aa7bf480ccc71dd60cc5102ffa6ce8017cbcd
Alessio Acampora <acampora199@gmail.com>
2022-07-19 16:26:32 +0100

    added custom inspector

---
+++ hamletDDA/Assets/Editor/DDAInspector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(DDAManager))]
+public class DDAInspector : EditorWindow
+{
+    string name = " ";
+    DDAManager targetManager;
+    DataCollector targetCollector;
+
+    [MenuItem("Window/Hamlet DDA")]
+
+    public static void ShowWindow()
+    {
+        DDAInspector window = EditorWindow.GetWindow<DDAInspector>();
+        window.Show();
+    }
+    void OnGUI()
+    {
+        GUILayout.Label("Manager Settings", EditorStyles.boldLabel);
+        targetManager = (DDAManager)EditorGUILayout.ObjectField(targetManager, typeof(DDAManager), true);
+
+        GUILayout.Label("Observers Settings", EditorStyles.boldLabel);
+        name = EditorGUILayout.TextField("name of Observer", name);
+        targetCollector = (DataCollector)EditorGUILayout.ObjectField(targetCollector, typeof(DataCollector), true);
+
+
+        if (GUILayout.Button("Insert new Observer Value"))
+        {
+            //targetManager.InsertSurveryor(name, targetCollector);
+        }
+    }
+}
+
---
+++ hamletDDA/Assets/scripts/DDAManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DDAManager : MonoBehaviour
+{
+    Dictionary<string, float> Inspectors = new Dictionary<string, float>();
+
+    public void AddInspector(string name, float value)
+    {
+        Inspectors.Add(name, value);
+    }
+}
+
